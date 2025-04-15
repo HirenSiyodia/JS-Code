5d79efad2487b5231ba8f48fa9821ae2084103f2
HirenSiyodia <siyodiahiren7@gmail.com>
2025-04-15 07:35:58 +0000

    Object1

--- Objects/objects.js
+++ Objects/objects.js
@@ -1,12 +1,30 @@
 //singleton
 
 //Objetcs Literal
+const mySymbl = Symbol('key1')
 const JsUser = {
     name: "hiren",
     age: 18,
+    [mySymbl]: "myKey1",
     email:"hiren@gmail.com",
     isLoggedIn: false,
     lastLoggedIn: ["monday", "tuesday", "wednesday"]
  }
 // Dot Notation
 console.log(JsUser.name);
+//Square Notation
+console.log(JsUser["age"]);
+console.log(JsUser[mySymbl]);
+
+// Functions
+// let greeting = function(){
+//     console.log("hirennnnnn");
+// }
+// greeting()
+
+// using 'this' => always use "backtick" its called sting interpolican
+JsUser.greetingOne = function(){
+    console.log(`hiren user, ${this.email}`);
+}
+
+console.log(JsUser.greetingOne());
