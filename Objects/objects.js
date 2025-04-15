7c65c20625d71f3ae573feca6c346b5b950d67b6
HirenSiyodia <siyodiahiren7@gmail.com>
2025-04-15 06:56:48 +0000

    container config done

---
+++ Objects/objects.js
@@ -0,0 +1,12 @@
+//singleton
+
+//Objetcs Literal
+const JsUser = {
+    name: "hiren",
+    age: 18,
+    email:"hiren@gmail.com",
+    isLoggedIn: false,
+    lastLoggedIn: ["monday", "tuesday", "wednesday"]
+ }
+// Dot Notation
+console.log(JsUser.name);
