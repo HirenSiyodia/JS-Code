53af8b11e9756d2bb14eade4676cdd82db34cc91
Hiren Siyodia | Front End Developer <siyodiahiren7@gmail.com>
2025-04-16 20:37:40 +0530

    Update break-continue.js

--- Break-n-Continue/break-continue.js
+++ Break-n-Continue/break-continue.js
@@ -1,11 +1,11 @@
 // Break will Break the execution from given value and after break nothing will print.
-// for (let index = 0; index <= 20; index++) {
-//     if (index == 9) {
-//         console.log("Numer 9 Detected..");
-//         break;
-//     }
-//     console.log(`Value is: ${index}`);   
-// }
+for (let index = 0; index <= 20; index++) {
+    if (index == 9) {
+        console.log("Numer 9 Detected..");
+        break;
+    }
+    console.log(`Value is: ${index}`);   
+}
 
 // Continue will resume his work after a pause. It will allow user to print a further data.
 for (let index = 1; index <= 10; index++) {
