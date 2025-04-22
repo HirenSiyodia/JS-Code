e6558b7b6e8a3910809caf8b71dd587408a7a197
Hiren Siyodia | Front End Developer <siyodiahiren7@gmail.com>
2025-04-22 07:00:44 +0000

    For of Loop

---
+++ Iterations/for_of.js
@@ -0,0 +1,24 @@
+// Syntax of for of loop
+// for (const element of object) {
+// }
+
+//Using on Array
+let myArray = [1, 2, 3, 4, 5]
+
+for (const arr of myArray) {
+    // console.log(`My Array Value is: ${arr}`);
+}
+
+// o/p:- My Array Value is: 1
+// My Array Value is: 2
+// My Array Value is: 3
+// My Array Value is: 4
+// My Array Value is: 5
+
+
+//Using on String
+let Greetings = "Hello Hiren";
+
+for (const greet of Greetings) {
+    // console.log(`Value is: ${greet}`);
+}
